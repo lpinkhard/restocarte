606780aa71d41c934cb51937bdcd6b31d3b1611d
Lionel <lionel@infinitycurve.ae>
2023-05-07 21:47:16 +0400

    Base url for sign up

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -14,14 +14,15 @@ export default function Subscribe (plan) {
                 break;
         }
         const stripe = await loadStripe('pk_test_51KtYPUJlwbqaAWHsXdJc7kAIYQIqpFgi0hdecopXGCOoMxCQSW90w7UrzSx0LIrtTRNKuQXRhJ4yHqa9f6HA0xTl00usLrEtJX')
+        const baseUrl = window.location.protocol + '//' + window.location.host + '/';
         const { error } = await stripe.redirectToCheckout({
             lineItems: [{
                 price: price_key,
                 quantity: 1
             }],
             mode: 'subscription',
-            successUrl: 'http://localhost:3000/',
-            cancelUrl: 'http://localhost:3000/cancel'
+            successUrl: baseUrl,
+            cancelUrl: baseUrl + 'sign-up',
         })
     }
 
