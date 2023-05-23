86e7fd71f07228e67b22526f48077ccd621c5603
Lionel <lionel@infinitycurve.ae>
2023-05-23 23:59:46 +0400

    Powered by link and styles

--- src/Present.js
+++ src/Present.js
@@ -1,10 +1,11 @@
-import {View} from '@aws-amplify/ui-react';
+import {Text, View} from '@aws-amplify/ui-react';
 
 import MainHeading from './MainHeading';
 import Menu from './Menu';
 import React, {useCallback, useState} from "react";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {hasWebPSupport} from "./Helpers";
+import {useTranslation} from "react-i18next";
 
 const Present = () => {
     const { restaurantId } = useParams();
@@ -19,16 +20,19 @@ const Present = () => {
         setContentReady(val);
     }, [setContentReady]);
 
+    const {t} = useTranslation();
+
     const webp = hasWebPSupport();
 
-    return (
+    return ([
         <View className="Present">
             <MainHeading restaurantId={restaurantId} loadRestaurant={restaurantLoaded} contentReady={onContentReady} webp={webp} displayTagline isMenu />
             {contentReady && (
                 <Menu restaurant={restaurant} webp={webp} />
             )}
-        </View>
-    );
+        </View>,
+        <Text fontSize="0.75rem" textAlign="center"><Link to="https://www.restocarte.com">{t('powered-by-restocarte')}</Link></Text>
+    ]);
 };
 
 export default Present;
